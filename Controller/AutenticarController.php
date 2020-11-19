18edffa8a6b61a8cba7757ce5dbdab71f303babe
João Guedes <joao.guedes@academico.ifrn.edu.br>
2020-11-19 10:44:44 -0300

    Conexão com o banco de dados

--- Controller/AutenticarController.php
+++ Controller/AutenticarController.php
@@ -1,27 +1,34 @@
 <?php
-    require "SessionController.php";
+// session_start inicia a sessão
+session_start();
+// as variáveis login e senha recebem os dados digitados na página anterior
+$email = $_POST['email'];
+$senha = $_POST['senha'];
+// as próximas 3 linhas são responsáveis em se conectar com o bando de dados.
+$con =  mysqli_connect("127.0.0.1", "root", "", "Estoque")  or die
+ ("Sem conexão com o servidor");
 
-    session_start();
+// A variavel $result pega as varias $login e $senha, faz uma
+//pesquisa na tabela de usuarios
+$result = mysqli_query("SELECT * FROM `Usuarios`
+WHERE `email` = '$email' AND `senha`= '$senha'");
+/* Logo abaixo temos um bloco com if e else, verificando se a variável $result foi
+bem sucedida, ou seja se ela estiver encontrado algum registro idêntico o seu valor
+será igual a 1, se não, se não tiver registros seu valor será 0. Dependendo do
+resultado ele redirecionará para a página site.php ou retornara  para a página
+do formulário inicial para que se possa tentar novamente realizar o login */
+if(mysqli_num_rows($result) > 0 )
+{
+$_SESSION['email'] = $email;
+$_SESSION['senha'] = $senha;
+header('location:../View/home.php');
+}
+else{
+    echo $_SESSION['email'];
+    echo $_SESSION['senha'];
+  unset ($_SESSION['email']);
+  unset ($_SESSION['senha']);
+  //header('location:../index.php');
 
-    $email = $_POST['email'];
-
-    $senha = $_POST['senha'];
-
-    echo $email;
-    echo $senha;
-
-
-    $sql = "SELECT * FROM Usuarios WHERE email = {$email} AND senha = {$senha};"
-    
-    $sql = @mysql_fetch_array($sql);
-
-    echo $sql;
-    
-    if ($sql) {
-        $_SESSION['User'] = @mysql_fetch_array($sql);
-        header("Location: ../View/home.php");
-    } else {
-        alert("E-mail ou senha incorretos!!!");
-        header("Location: index.php");
-    }
+  }
 ?>
